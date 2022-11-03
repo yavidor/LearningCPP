8250c98cceb8989eaa5b937efc41966ba0f149b7
yavidor <yonatan@avidor.org>
2022-11-03 10:27:13 +0200

    The compiler works

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout;
+int main()
+{
+    int x, quantity;
+    float price;
+    x=-52;
+    quantity=10;
+    price=27.31;
+    cout<<"x = "<<x<<" quantity = "<<quantity<<" price = "<<price<<'\n';
+    return 0;
+}
